7754277246c409ad627216616b76def3155c7711
codeWithSilver <61716921+codeWithSilver@users.noreply.github.com>
2020-11-28 18:32:44 -0500

    starter project

---
+++ lib/config/palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/painting.dart';
+
+class Palette {
+ static const Color iconColor = Color(0xFFB6C7D1);
+ static const Color activeColor = Color(0xFF09126C);
+ static const Color textColor1 = Color(0XFFA7BCC7);
+ static const Color textColor2 = Color(0XFF9BB3C0);
+ static const Color facebookColor = Color(0xFF3B5999);
+ static const Color googleColor = Color(0xFFDE4B39);
+ static const Color backgroundColor = Color(0xFFECF3F9);
+}
---
+++ lib/screen/login_signup.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class LoginSignupScreen extends StatefulWidget {
+  @override
+  _LoginSignupScreenState createState() => _LoginSignupScreenState();
+}
+
+class _LoginSignupScreenState extends State<LoginSignupScreen> {
+  bool isMale = true;
+  bool isSignupScreen = true;
+  bool isRememberMe = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
